5945c63f017fe0474fda6ab55b89c7da8ec4fcea
lsdun <lasdun@syr.edu>
2025-09-23 09:07:32 -0400

    4th Order Runge-Kutta Method

--- Integrators.py
+++ Integrators.py
@@ -7,6 +7,10 @@ Definite Integral Solvers: Riemann sum, the trapezoidal rule, Simpson's rule.
 
 """
 
+import numpy as np
+
+# Explicit Euler Method:
+
 def euler_method(func, t, S, h):
     """
    Perform one step of Euler's method for an ODE.
@@ -21,3 +25,25 @@ def euler_method(func, t, S, h):
         ndarray: State vector at next step. 
     """
     return S + h * func(t, y)
+
+# Fourth-Order Runge-Kutta Method:
+
+def rk4_step(func, t, S, h):
+    """
+    Perform one step of the 4th-order Runge-Kutta method for an ODE.
+
+    Args:
+        func (callable): Function.
+        t (float): Current time.
+        S (ndarray): Current state vector.
+        h (float): Time step. 
+
+    Returns:
+        ndarray: State vector at next step. 
+    """
+    k1 = func(t, S)
+    k2 = func(t + h/2, S + 1/2 * k1 * h)
+    k3 = func(t + h/2, S + 1/2 * k2 * h)
+    k4 = func(t + h, S + k3 * h)
+
+    return S + (h/6) * (k1 + 2 * k2 + 2 * k3 + k4)
