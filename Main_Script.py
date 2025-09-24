797c4f2df2312695df463025bceda54154c8a0e2
lsdun <lasdun@syr.edu>
2025-09-24 07:35:50 -0400

    Implementing Analytic and SciPy Methods

--- Main_Script.py
+++ Main_Script.py
@@ -63,22 +63,10 @@ def energy (S, m, k):
 E_euler = energy(S_vals_euler, m, k)
 E_rk4 = energy(S_vals_rk4, m, k)
 
-# plot energy
-plt.figure(figsize=(10, 7))
-plt.plot(t_vals, E_euler, label='Euler Energy', color="mediumvioletred")
-plt.plot(t_vals, E_rk4, label='RK4 Energy', color="teal")
-plt.xlabel("Time [s]", fontsize=15)
-plt.ylabel('Total Energy E(t) [J]', fontsize=15)
-plt.title('Damped Harmonic Oscillator Energy Decay', fontsize=20)
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
-plt.show()
-
-# Try different damping values to show under, critical, and overdamping
+# try different damping values to show under, critical, and overdamping
 gamma_values = [0.2, 2.0, 5.0]  # under, critical, over
 labels = ["Underdamped", "Critically Damped", "Overdamped"]
-colors = ["mediumvioletred", "teal", "lightgreen"]
+colors = ["mediumvioletred", "teal", "mediumseagreen"]
 
 plt.figure(figsize=(10, 7))
 for gamma, label, color in zip(gamma_values, labels, colors):
@@ -93,12 +81,66 @@ for gamma, label, color in zip(gamma_values, labels, colors):
         return [dxdt, dvdt]
     
     t_vals, S_vals = rk4_loop(damped_gamma, S0, t0, t1, h)
-    plt.plot(t_vals, S_vals[:, 0], label=label, color=color)
+    plt.plot(t_vals, S_vals[:, 0], label=label, lw =2, color=color)
 
 plt.xlabel("Time [s]", fontsize=15)
 plt.ylabel("Displacement x(t)", fontsize=15)
-plt.title("Damped Harmonic Oscillator in Different Regimes", fontsize=20)
+plt.title("Damped Harmonic Oscillator Damping Ratio", fontsize=20)
 plt.legend()
 plt.grid(True)
 plt.tight_layout()
+plt.show()
+
+# calculate the analytic solution
+x_exact = analytic_damped(t_vals, m, k, gamma, x0=1, v0=0) # position
+v_exact = np.gradient(x_exact, h) # derivative of position
+t_vals = np.arange(t0, t1 + h, h)
+
+# calculate the energy for the exact solution: 
+S_exact = np.column_stack((x_exact, v_exact))
+E_exact = energy(S_exact, m, k)
+
+# call SciPy's ODE solver
+# solving an initial value problem for an ODE system
+sol = solve_ivp(
+    lambda t, S: damped(S, t, m=m, gamma=gamma, k=k), # RHS of the ODE
+    [t0, t1], # time interval
+    S0, # initial condition
+    t_eval = t_vals, # times to return solution
+    method = 'RK45', #explicit rk method 4th order
+)
+
+# extract position and velocity
+x_scipy = sol.y[0] # position is the first component
+v_scipy = sol.y[1] # velocity is the second component
+S_scipy = np.column_stack((x_scipy, v_scipy))
+E_scipy = energy(S_scipy, m, k)
+
+# Compare analytic vs numerical solutions by plotting displacement
+
+plt.figure(figsize=(10,6))
+plt.plot(t_vals, x_exact, 'k--', lw=3, label='Analytic')
+plt.plot(t_vals, S_vals_euler[:, 0], color="mediumvioletred", lw=2, label='Euler')
+plt.plot(t_vals, S_vals_rk4[:, 0], color="teal", lw=2, label='RK4')
+plt.plot(t_vals, x_scipy, color="skyblue", lw=2, label='SciPy RK45')
+plt.xlabel('Time t [s]', fontsize=14)
+plt.ylabel('Displacement x(t)', fontsize=14)
+plt.title('Damped Harmonic Oscillator: Analytic vs Numeric', fontsize=16)
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+# Plotting the energy for all 4 methods
+
+plt.figure(figsize=(10,6))
+plt.plot(t_vals, E_exact, 'k--', lw=3, label="Analytic")
+plt.plot(t_vals, E_euler, color="mediumvioletred", lw=2, label="Euler")
+plt.plot(t_vals, E_rk4, color="teal", lw=2, label="RK4")
+plt.plot(t_vals, E_scipy, color="skyblue", lw=2, label="SciPy RK45")
+plt.xlabel("Time [s]", fontsize=15)
+plt.ylabel('Total Energy E(t) [J]', fontsize=15)
+plt.title('Damped Harmonic Oscillator Total Mechanical Energy', fontsize=20)
+plt.legend()
+plt.grid(True)
 plt.show()
