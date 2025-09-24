16177f51895a19cee52987317085d6cd23630233
lsdun <lasdun@syr.edu>
2025-09-24 15:46:22 -0400

    Biot Savart Analytic Solution

--- Analytic_Solutions.py
+++ Analytic_Solutions.py
@@ -1,4 +1,7 @@
-# Analytic_Solutions.py
+"""
+This file contains the analytic solutions to the damped harmonic oscillator 
+and the magnetic field of a finite current carrying wire using Biot-Savart law.
+"""
 import numpy as np
 
 def analytic_damped(t, m, k, gamma, x0, v0):
@@ -41,3 +44,31 @@ def analytic_damped(t, m, k, gamma, x0, v0):
         x = A*np.exp(r1*t) + B*np.exp(r2*t)
 
     return x
+
+# analytic solution for the magnetic field of a current carrying wire
+
+mu0 = 4 * np.pi * 1e-7 # permeability of free space (H/m)
+I = 1.0 # current (A)
+R = 0.1 # perpendicular distance to wire (m)
+
+# wire lengths to evaluate
+L_values = np.linspace(0.1, 10, 50) # in meters
+
+mu0 = 4 * np.pi * 1e-7  # permeability of free space (H/m)
+
+def biotsavart_analytic(I, R, L_values):
+    """
+    Compute the analytic magnetic field of a finite straight wire along the z-axis
+    at perpendicular distance R.
+
+    Args:
+        I (float): Current in the wire (A).
+        R (float): Perpendicular distance from wire (m).
+        L_values (array_like): Array of wire lengths to evaluate (m).
+
+    Returns:
+        np.ndarray: Magnetic field B_y for each wire length.
+    """
+    L_values = np.array(L_values)
+    B_analytic = (mu0 * I) / (4 * np.pi * R) * (L_values / np.sqrt(R**2 + (L_values / 2)**2))
+    return B_analytic
